d3de208adf469de1f88ce863fb8e125431664da2
Arielle Yoo <arielleyoo@campus-113-010.ucdavis.edu>
2022-04-18 14:46:36 -0700

    Changed default to true temporarily

--- run_model_selection.py
+++ run_model_selection.py
@@ -105,7 +105,7 @@ def preprocess(
 @click.option(
     '--load-data-preprocessed',
     type=bool,
-    default=False)
+    default=True) #TODO remove - Load preprocessed data by default for one run
 @click.option(
     '--random-state',
     type=int,
