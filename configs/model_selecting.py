9d25f46f10e80abd8cc68dfae397374aaaf78aca
Arielle Yoo <arielleyoo@campus-030-127.ucdavis.edu>
2022-05-23 19:44:38 -0700

    Added scripts for more ages of model selection

--- configs/model_selecting.py
+++ configs/model_selecting.py
@@ -39,11 +39,11 @@ class DefaultConfig:
         PATH_ROOT + "/output/sfs_analysis")
 
     #COLUMN_DEPENDENT = 'y12CH_Dep_YN_144m'
-    COLUMN_DEPENDENT = 'y13CH_Dep_YN_162m'
+    #COLUMN_DEPENDENT = 'y13CH_Dep_YN_162m'
     #COLUMN_DEPENDENT = 'y16CH_Dep_YN_192m'
     #COLUMN_DEPENDENT = 'y17CH_Dep_YN_204m'
     #COLUMN_DEPENDENT = 'y18CH_Dep_YN_216m'
-    #COLUMN_DEPENDENT = 'y12to18_Dep_YN_216m'
+    COLUMN_DEPENDENT = 'y12to18_Dep_YN_216m'
 
     # AGE_CUTOFF = 18 # year from column_dependent
     # AGE_CUTOFF = 12
