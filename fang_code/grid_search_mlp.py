4b5e713aaf8bcc9ffcb18cfc287e37476f1d2c77
Arielle Yoo <arielleyoo@Arielles-MacBook-Pro.local>
2022-06-24 02:21:52 -0700

    Fixed function tolist() for ndarray

--- fang_code/grid_search_mlp.py
+++ fang_code/grid_search_mlp.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     num_hidden_neurons_end = 101
     num_hidden_neurons_increment = 10
     param_grid = {
-        'alpha':  np.logspace(-1, 2, 5).tolist,
+        'alpha':  np.logspace(-1, 2, 5).tolist(),
         'max_iter': list(
             range(num_max_iter_start, num_max_iter_end,
                   num_max_iter_increment)),
